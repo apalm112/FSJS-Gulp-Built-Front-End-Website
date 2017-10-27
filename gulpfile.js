0f55cd1e6797be2554cdc43c1b156f2b28060669
sofa_king <apalm112@gmail.com>
2017-10-26 22:26:11 -0700

    Working on bug, right now the CSS source maps work & on .scss file change.  all.min.js is working, but all.min.js.map is NOT working.

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,24 @@ const options = {
 };
 
 /* Gulp Tasks      *********************************************************/
-gulp.task('scripts', ['jsMinify'], ()=> {
-	// Runs jsMinify task first, then creates source map, concats the two JS files.
-	return gulp.src([options.src + '/js/circle/autogrow.js', options.src + '/js/global.js'])
+// Runs jsMinify task first, then creates source map, concats the two JS files.
+gulp.task('scripts', ()=> {
+	return gulp.src([options.src + '/js/circle/autogrow.js', options.src + '/js/circle/circle.js'])
+		.pipe(uglify())
+		.pipe(rename('global.js'))
+		.pipe(gulp.dest(options.src + '/js/'))
 		.pipe(maps.init())
 		.pipe(concat('all.min.js'))
 		.pipe(maps.write('./'))
 		.pipe(gulp.dest(options.dist + '/scripts/'));
 });
-gulp.task('jsMinify', ()=> {
+/*gulp.task('jsMinify', ()=> {
 	//	Minifies the one JS file that needs it, since js/autogrow.js is already minified.
 	return gulp.src(options.src + '/js/circle/circle.js')
 	.pipe(uglify())
 	.pipe(rename('global.js'))
 	.pipe(gulp.dest(options.src + '/js/'));
-});
+});*/
 
 gulp.task('cssMinify', ()=> {
 	return gulp.src(options.src + '/sass/global.scss')
@@ -44,8 +47,8 @@ gulp.task('cssMinify', ()=> {
 		.pipe(gulp.dest(options.src + '/css/'))
 });
 
+// Needed in order to meet rubric for styles task to compile, concat & minify all SaSS files.
 gulp.task('styles', ['cssMinify'], ()=> {
-	// Needed in order to meet rubric for styles task to compile, concat & minify all SaSS files.
 	return gulp.src(options.dist + '/styles/global.css')
 		.pipe(csso())
 		.pipe(rename('all.min.css'))
@@ -64,8 +67,8 @@ gulp.task('clean', ()=> {
 	return del(['dist/*', 'src/css/', 'src/js/global.js']);
 });
 
+// Takes the index.html file & runs it thru useref().  Runs the clean, scripts, styles & images tasks as dependencies.  Then any JS scripts & CSS links in the index.html get concated, minified for production.
 gulp.task('html', ['scripts', 'styles', 'images'], ()=> {
-	// Runs the clean, scripts, styles & images tasks as dependencies.  Then any JS scripts & CSS links in the index.html get concated, minified for production.
 	return gulp.src(options.src + '/index.html')
 		.pipe(iff('*.js', uglify()))
 		.pipe(iff('*.css', csso()))
@@ -73,8 +76,8 @@ gulp.task('html', ['scripts', 'styles', 'images'], ()=> {
 		.pipe(gulp.dest(options.dist));
 });
 
+//  Runs the clean, scripts, styles, and images tasks. Setups the project development files into a folder for production.
 gulp.task('build', ['clean'], ()=> {
-	//  Runs the clean, scripts, styles, and images tasks. Setups the project development files into a folder for production.
 	gulp.start('html');
 	// Provide production files below:
 	return gulp.src(['src/index.html',
@@ -93,8 +96,8 @@ gulp.task('default', ['build'], ()=> {
 	gulp.watch(options.src + '/sass/**/*.scss', ['watchMinify']);
 });
 
+// This task runs 'styles' as dependency, & then itself is run as a dependency of the 'watch' task.  Then it will minify the global.css file w/ csso(), update the production directory file & update the browser, displaying the changes.
 gulp.task('watchMinify', ['styles'], ()=> {
-	// This task runs 'styles' as dependency, & then itself is run as a dependency of the 'watch' task.  Then it will minify the global.css file w/ csso(), update the production directory file & update the browser, displaying the changes.
 	return gulp.src(options.src + '/index.html')
 		.pipe(iff('*.js', uglify()))
 		.pipe(iff('*.css', csso()))
