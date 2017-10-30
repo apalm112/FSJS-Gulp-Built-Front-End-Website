0de0f55638c20ffc63c2ae93036e382ae8661b0e
sofa_king <apalm112@gmail.com>
2017-10-30 14:52:53 -0700

    Finished refactoring.  All gulp tasks now work per project rubric, including the push goal of gulp watch task w/ live reload feature in the browser.

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,7 @@ const gulp = require('gulp'),
 	del = require('del'),
 	browserSync = require('browser-sync').create(),
 	runSequence = require('run-sequence'),
-	pump = require('pump'),  // pump is a wrapper for .pipe that gives moar readable error messages.
-	QPromise = require('q');  //	https://www.npmjs.com/package/q
+	pump = require('pump');  // pump is a wrapper for .pipe that gives moar readable error messages.
 
 // Variables w/ path to source & dist folders.
 const options = {
@@ -23,40 +22,40 @@ const options = {
 };
 
 /* Gulp Tasks      *********************************************************/
-gulp.task('scripts', (callback) => {
+gulp.task('jsMinify', (callback) => {
 	pump([
-		gulp.src([options.src + '/js/circle/*.js', options.src + '/js/global.js']),
+		gulp.src(options.src + '/js/circle/*.js'),
 		maps.init(),
-		concat('all.min.js'),
+		concat('global.js'),
 		uglify(),
 		maps.write('./'),
-		gulp.dest(options.dist + '/scripts/'),
+		gulp.dest(options.src + '/js/')
 	],
 	callback
 	);
 });
+gulp.task('moveAllJS', ['jsMinify'], (callback) => {
+	pump([
+		gulp.src(options.src + '/js/global.js'),
+		rename('all.min.js'),
+		gulp.dest(options.dist + '/scripts/')
+	],
+	callback
+	);
+});
+gulp.task('scripts', ['moveAllJS'], () => {
+	gulp.src(options.src + '/js/all.min.js');
+	del(options.src + '/js/all.min.js');
+});
+
 
 gulp.task('cssMinify', (callback) => {
 	pump([
 		gulp.src(options.src + '/sass/global.scss'),
-		rename('all.min.css'),
 		maps.init({ largeFile: true }),
 		sass(),
 		maps.write('./'),
-		gulp.dest(options.dist + '/styles/'),
-		gulp.dest(options.src + '/css/')
-	],
-	callback
-	);
-});
-
-gulp.task('styles', ['cssMinify'], (callback) => {
-	pump([
-		gulp.src(options.dist + '/styles/global.css'),
-		rename('all.min.css'),
-		csso(),  // Minifies
-		gulp.dest(options.dist + '/styles/'),
-		gulp.dest(options.src + '/sass/'),
+		gulp.dest(options.src + '/css/'),
 		browserSync.reload({
 			stream: true
 		})
@@ -65,38 +64,27 @@ gulp.task('styles', ['cssMinify'], (callback) => {
 	);
 });
 
-gulp.task('images', (callback) => {
-	// Uses gulp-imagemin module to optimize the images for production.
+gulp.task('styles', ['cssMinify'], (callback) => {
 	pump([
-		gulp.src(options.src + '/images/*'),
-		imagemin(),
-		gulp.dest(options.dist + '/content')
+		gulp.src(options.src + '/css/global.css'),
+		csso(),  // Minifies
+		rename('all.min.css'),
+		gulp.dest(options.dist + '/styles/'),
 	],
 	callback
 	);
+});
+
+gulp.task('images', () => {
+	// Uses gulp-imagemin module to optimize the images for production.
+	return gulp.src(options.src + '/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest(options.dist + '/content'));
 });
 
 gulp.task('clean', () => {
 	// Deletes all of the files and folders in the dist folder created from tasks.
-	return del(['dist/*']);
-});
-gulp.task('cleanUp', () => {
-	// Remove unwanted file from the production folder.
-	del(options.dist + '/styles/global.css');
-});
-
-// Takes the index.html file & runs it thru useref(). Then any JS scripts & CSS links in the index.html get concated, minified for production.
-gulp.task('html', (callback)=> {
-	// TODO: BUG! USE A MODULE, (QPROMISE, RUN-SEQUENCE) TO CHANGE ORDER OF TASKS TO SYNCHRONOUS.
-	pump([
-		gulp.src(options.src + '/index.html'),
-		iff('*.js', uglify()),
-		iff('*.css', csso()),
-		useref(),		//  Clearly, this is where the useref() belongs.
-		gulp.dest(options.dist)
-	],
-	callback
-	);
+	return del(['dist/*', 'src/css/', 'src/js/global.js.map', 'src/js/global.js']);
 });
 
 //  Setups the project development files into a folder for production.
@@ -104,10 +92,11 @@ gulp.task('build', (callback) => {
 	runSequence('clean',
 							['scripts', 'styles', 'images', 'buildOut'],
 							callback
-	)
+	);
 });
 
 gulp.task('buildOut', () => {
+	// Provides the static development files for the production folder.
 	return gulp.src(['src/icons/**/*',
 									'src/index.html'],
 									{ base: './src/' })
@@ -116,21 +105,16 @@ gulp.task('buildOut', () => {
 
 gulp.task('default', (callback) => {
 	runSequence('build',
-							['watcher', 'html'],
+							['server'],
 							callback
-	)
+	);
 });
 
 gulp.task('server', () => {
 	browserSync.init({
 		server: {
-			baseDir: 'dist'
+			baseDir: 'src/'
 		}
 	});
-});
-
-gulp.task('watcher', (callback) => {
 	gulp.watch(options.src + '/sass/**/*.scss', ['styles']);
-	runSequence('server', callback);
-	//	'cleanUp',
 });
