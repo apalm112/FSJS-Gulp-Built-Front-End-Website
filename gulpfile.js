355d3165f6e04a45861b7c8f5f16d3852bdfb2e7
sofa_king <apalm112@gmail.com>
2017-10-31 01:42:22 -0700

    Added live-reload feature for changes to CSS & JS src files.

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ const gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	imagemin = require('gulp-imagemin'),
 	del = require('del'),
+	iff = require('gulp-if'),
+	useref = require('gulp-useref'),
 	browserSync = require('browser-sync').create(),
 	runSequence = require('run-sequence'),
 	pump = require('pump');  // pump is a wrapper for .pipe() that gives moar readable error messages.  https://github.com/gulpjs/gulp/blob/master/docs/why-use-pump/README.md
@@ -38,7 +40,11 @@ gulp.task('scripts', ['jsMinify'], (callback) => {
 	pump([
 		gulp.src(options.src + '/js/global.js'),
 		rename('all.min.js'),
-		gulp.dest(options.dist + '/scripts/')
+		gulp.dest(options.dist + '/scripts/'),
+		// This browserSync Live-reloads when project is run from src/
+		browserSync.reload({
+			stream: true
+		})
 	],
 	callback
 	);
@@ -52,6 +58,7 @@ gulp.task('cssMinify', (callback) => {
 		sass(),
 		maps.write('./'),
 		gulp.dest(options.src + '/css/'),
+		// This browserSync Live-reloads when project is run from src/
 		browserSync.reload({
 			stream: true
 		})
@@ -67,6 +74,10 @@ gulp.task('styles', ['cssMinify'], (callback) => {
 		csso(),
 		rename('all.min.css'),
 		gulp.dest(options.dist + '/styles/'),
+		// This browserSync Live-reloads when project is run from dist/
+		browserSync.reload({
+			stream: true
+		})
 	],
 	callback
 	);
@@ -86,14 +97,14 @@ gulp.task('clean', () => {
 
 gulp.task('build', (callback) => {
 	//  Runs tasks in sequence to create files for the production folder.
-	runSequence('clean', ['scripts', 'styles', 'images', 'buildOut'],
+	runSequence('clean', ['scripts', 'styles', 'images', 'buildOut'], 'useref',
 		callback
 	);
 });
 
 gulp.task('buildOut', () => {
 	//  Provides the static development files for the production folder.
-	gulp.src(['src/icons/**/*', 'src/index.html'], { base: './src/' })
+	gulp.src('src/icons/**/*', { base: './src/' })
 		.pipe(gulp.dest('dist'));
 });
 
@@ -111,14 +122,21 @@ gulp.task('server', () => {
 		}
 	});
 	gulp.watch(options.src + '/sass/**/*.scss', ['styles']);
-	gulp.watch(options.src + '/js/**/*.js', browserSync.reload);
+	gulp.watch(options.src + '/js/**/*.js', ['scripts']);
 	gulp.watch(options.dist + '/index.html', browserSync.reload);
 });
 
+gulp.task('useref', () => {
+	// Run the build tags in the .html file to combine the source files using useref().
+	return gulp.src(options.src + '/index.html')
+		.pipe(iff('*.js', uglify()))
+		.pipe(iff('*.css', csso()))
+		.pipe(useref())
+		.pipe(gulp.dest(options.dist));
+});
 /********************************************************************
-TODO: Refactor for:
-Don't forget that source maps don't need to be included in your html. They connect to each other through the css so there is no need to include it. If you delete it you will see that it works. Also i noticed that in your html file you are not including the minified version. This defeats the whole idea of concatenating and minifying doesn't it. This isn't asked of in this project(although before it was and they took it out) yet I think it's an important tool to know. So being able to change the css and js includes so that your production html will include the correct files is kinda important. Here is a link you might find useful incase you want to read up and add it to your project. It's actually super simple https://css-tricks.com/gulp-for-beginners/
+TODO: Refactor to get the Source Maps to work when production dist/ folder is run from local server.
+Here is a link you might find useful incase you want to read up and add it to your project. It's actually super simple https://css-tricks.com/gulp-for-beginners/
 
-You can also set up browser sync to work for your html too so if you changed some html it would also reload and build etc and you can add it for js too. Again not asked for in this project so just pointing it out incase you wanted to dig deeper and take better use of gulp for future projects or just general knowledge.
 
 ***************************************************************************/
